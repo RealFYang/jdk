053a76b6be05c41a380e3709d0bae5c1b08aa392
Fei Yang <yangfei@iscas.ac.cn>
2026-08-03 22:44:23 +0800

    [lworld] RISC-V: Value class adapter generation leaks CodeCache BufferBlob

--- src/hotspot/cpu/riscv/sharedRuntime_riscv.cpp
+++ src/hotspot/cpu/riscv/sharedRuntime_riscv.cpp
@@ -390,11 +390,10 @@ int SharedRuntime::java_return_convention(const BasicType *sig_bt,
 }
 
 BufferedInlineTypeBlob* SharedRuntime::generate_buffered_inline_type_adapter(const InlineKlass* vk) {
-  BufferBlob* buf = BufferBlob::create("inline types pack/unpack", 16 * K);
-  if (buf == nullptr) {
+  CodeBuffer buffer("inline types pack/unpack", 16 * K, 0);
+  if (buffer.blob() == nullptr) {
     return nullptr;
   }
-  CodeBuffer buffer(buf);
   short buffer_locs[20];
   buffer.insts()->initialize_shared_locs((relocInfo*)buffer_locs,
                                          sizeof(buffer_locs)/sizeof(relocInfo));
